d11cdaf4830fe98297d6321df3a751d362fe588b
PBalewski <balewskipiotrek@gmail.com>
2024-03-18 23:04:03 +0100

    '3rd_fragment'

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,3 +1,4 @@
+// (un)forgotten comment
 // Your First Program
 
 class HelloWorld {
@@ -5,5 +6,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 }
+// 3rd change
 // 2nd com
 // 3rd com
